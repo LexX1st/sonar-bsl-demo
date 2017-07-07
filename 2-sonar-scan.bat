0fc9c8d20c4fe5a27123ac7db76eb7734058e889
Alexey Lustin <alustin@silverbulleters.org>
2017-07-07 12:59:47 +0300

    Гарантированная ошибка на метод Сообщить()

--- 2-sonar-scan.bat
+++ 2-sonar-scan.bat
@@ -3,4 +3,4 @@ set CURPWD=%CURPWD:\=/%
 
 for /f "delims== tokens=1,2" %%G in (.env) do set %%G=%%H
 
-docker run -it --rm --env-file=.env -v "%CURPWD%":/gitrepo silverbulleters/sonar-history-runner sonar-runner -Dsonar.host.url=%SONAR_SERVER_URL% -Dsonar.login=%SONAR_TOKEN%
+docker run -it --rm --env-file=.env -v "%CURPWD%/.sonarcache":/root/.sonar/cache -v "%CURPWD%":/gitrepo silverbulleters/sonar-history-runner sonar-runner -Dsonar.host.url=%SONAR_SERVER_URL% -Dsonar.login=%SONAR_TOKEN%
